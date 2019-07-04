ab8faca3b7211f3ea6e5c6adc7568c75a1b55d77
Sunyz <15900313259@163.com>
2019-07-04 13:55:17 +0800

    'sourcemap和webpackserver'

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import _ from 'loadsh'
+import printMe from './print'
 import './style.scss'
 
 function component() {
     let element = document.createElement('div');
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello');
+    element.addEventListener('click', printMe, false);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,24 @@
 const path = require('path');
+//将import进去的css文件 也转换为css文件 而不是style
 const ExtractTextPlugin = require("extract-text-webpack-plugin")
+//自动生成index.html文件
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+//清理dist
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
+    },
+    devtool: 'inline-source-map',
+    //创建自己的本地服务器
+    devServer: {
+        contentBase: './dist'
     },
     module: {
         rules: [
@@ -20,5 +33,7 @@ module.exports = {
     },
     plugins: [
         new ExtractTextPlugin("style.css"),
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({title: "output management"})
     ]
 };
