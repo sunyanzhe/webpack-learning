ade844916be8fd4fb13adea4c7da8239a513bef1
sunyanzhe <sunyanzhe@jd.com>
2020-01-16 16:25:27 +0800

    plugin

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,19 +1,29 @@
 const path = require('path');
-
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
   mode: 'development',
   // javascript 执行入口文件
   entry: path.resolve(__dirname, '../src/main.js'),
   output: {
-    filename: 'bundle.js',
+    filename: 'js/bundle.js',
     path: path.resolve(__dirname, '../dist')    
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader']
+        use: ExtractTextPlugin.extract({
+          use: ['css-loader']
+        })
       }
     ]
-  }
+  },
+  plugins:[
+    new CleanWebpackPlugin(),
+    new ExtractTextPlugin({
+      //从.js文件中提取出来的.css的文件名称
+      filename: `css/[name]_[hash:8].css`,
+    }),
+  ]
 }
