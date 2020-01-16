008ac3179ac52452b419169c31fa420567faee1a
sunyanzhe <sunyanzhe@jd.com>
2020-01-16 14:53:41 +0800

    1-3 安装与使用

---
+++ build/webpack.config.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  // javascript 执行入口文件
+  entry: path.resolve(__dirname, '../src/main.js'),
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, '../dist')    
+  }
+}
