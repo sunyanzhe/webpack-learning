7ee998ad1ce61ed46c50a79afe3c17102657fdfc
sunyanzhe <sunyanzhe@jd.com>
2020-01-16 17:48:15 +0800

    source-map dev-sever

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -1,12 +1,13 @@
 const path = require('path');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 module.exports = {
   mode: 'development',
   // javascript 执行入口文件
   entry: path.resolve(__dirname, '../src/main.js'),
   output: {
-    filename: 'js/bundle.js',
+    filename: 'js/[name].[hash:8].js',
     path: path.resolve(__dirname, '../dist')    
   },
   module: {
@@ -19,11 +20,20 @@ module.exports = {
       }
     ]
   },
+  devServer: {
+    port: 3000,
+    hot: true,
+    contentBase: '../dist'
+  },
+  devtool: 'source-map',
   plugins:[
     new CleanWebpackPlugin(),
     new ExtractTextPlugin({
       //从.js文件中提取出来的.css的文件名称
       filename: `css/[name]_[hash:8].css`,
     }),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../index.html'),
+    })
   ]
 }
